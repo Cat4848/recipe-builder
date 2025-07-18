82337c9eb9ba0379284c151b6814a4cac813b109
Cat4848 <catalinrol@gmail.com>
2025-07-18 05:58:50 +0100

    test: if fails with position_x wrong data type

--- server/src/lib/validators/newNodeValidation/__tests__/newNodeValidation.test.ts
+++ server/src/lib/validators/newNodeValidation/__tests__/newNodeValidation.test.ts
@@ -22,3 +22,18 @@ test("if it fails with empty new node", async () => {
     expect(e).toBeInstanceOf(ValidationError);
   }
 });
+
+test("if it fails with wrong position_x data type", async () => {
+  expect.assertions(1);
+  const reqBody = {
+    position_x: new Set(),
+    position_y: 100,
+    type: "input",
+    content: "flour"
+  };
+  try {
+    await newNodeValidation(reqBody);
+  } catch (e) {
+    expect(e).toBeInstanceOf(ValidationError);
+  }
+});
