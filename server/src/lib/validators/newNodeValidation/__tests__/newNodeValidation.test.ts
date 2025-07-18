9b04590d9d91028bf6cd214dd85c78506b78ff87
Cat4848 <catalinrol@gmail.com>
2025-07-18 05:56:05 +0100

    test: if it fails with empty newNode

--- server/src/lib/validators/newNodeValidation/__tests__/newNodeValidation.test.ts
+++ server/src/lib/validators/newNodeValidation/__tests__/newNodeValidation.test.ts
@@ -1,6 +1,8 @@
 import { newNodeValidation } from "../newNodeValidation";
+import { ValidationError } from "yup";
 
 test("if it passes with a correct new node", async () => {
+  expect.assertions(1);
   const reqBody = {
     position_x: 0,
     position_y: 100,
@@ -8,7 +10,15 @@ test("if it passes with a correct new node", async () => {
     content: "flour"
   };
   const mockWrapper = jest.fn(async () => await newNodeValidation(reqBody));
-  expect(mockWrapper).not.toThrow()
+  expect(mockWrapper).not.toThrow();
 });
 
-
+test("if it fails with empty new node", async () => {
+  expect.assertions(1);
+  const reqBody = {};
+  try {
+    await newNodeValidation(reqBody);
+  } catch (e) {
+    expect(e).toBeInstanceOf(ValidationError);
+  }
+});
